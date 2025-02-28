148db6974cc2928f48e0ca9803a398f5a7b0ed5c
Vaskalaz <thanapat30769@gmail.com>
2025-02-28 21:04:23 +0700

    Update lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 //Write function count() here.
+int count(int data[],int N){
+    set<int> num(data, data+N);
+    return num.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
